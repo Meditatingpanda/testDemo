44a67b31ad9c2f076635111bb15ac941029a25da
Traveling Monk <grp.gyanaranajan@gmail.com>
2022-01-09 10:06:17 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let btn=document.querySelector('.btn');
 let ul=document.querySelector('ul');
 let ulList=document.querySelectorAll('li');
 
+let body=document.querySelector('body');
+body.style.backgroundImage=`url(./bgSmall.jpg)`;
 input.addEventListener('keyup',(e)=>{
    if(e.keyCode==13){
      btn.click();
@@ -13,18 +15,14 @@ btn.addEventListener('click',()=>{
 //  ul.innerHTML+=`<li class=" listStyle">${input.value}</li>`;
 //  input.value='';
  let li =document.createElement('li');
- li.innerText='* '+input.value;
+ if(input.value.length!==0){
+  li.innerHTML='<i class="fas fa-check"></i>'+input.value;
+ }
  input.value='';
  li.setAttribute('class','shadow-sm pl-3 mb-3 hover:shadow-md ');
  ul.append(li);
 })
 
-// ulList.forEach(key=>{
-//   key.addEventListener('click',(e)=>{
-//    e.target.remove();
-
-//   })
-// })
 ul.addEventListener('click',(e)=>{
   console.log(e.target.tagName);
   if(e.target.tagName=="LI")
